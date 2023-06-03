914181b882c4a075b156dfadb8046ce66ffcaa84
khorshed-alam-kbd <khorshedalam.kbd@gamil.com>
2023-06-04 01:00:02 +0600

    khorshedalam-kbd

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -10,7 +10,7 @@ const Skills = () => {
                 <div className="card-body">
                     <p className='flex gap-3 items-center'><FaEdit /><span><span className='font-bold'>Expertise: </span> <br /><span className='text-gray-400'>HTML5 | CSS3 | ES6 | JavaScript | React.js | Bootstrap | React Bootstrap | Tailwind CSS | DaisyUI. </span></span></p>
                     <p className='flex gap-3 items-center'><FaEdit /> <span> <span className='font-bold'>Comfortable:</span> <br /> <span className='text-gray-400'>Express.js | Node.js | MongoDB | JSON | REST Api | React Query | React Hook Form.</span> </span></p>
-                    <p className='flex gap-3 items-center'><FaEdit /><span><span className='font-bold'>Familiar:</span> <br /><span className='text-gray-400'>Next.js | JWT Token | Axios.</span> </span></p>
+                    <p className='flex gap-3 items-center'><FaEdit /><span><span className='font-bold'>Familiar:</span> <br /><span className='text-gray-400'>Next.js | JWT Token | Axios | Redux.</span> </span></p>
                     <p className='flex gap-3 items-center'><FaEdit /><span><span className='font-bold'> Tools:</span> <br /><span className='text-gray-400'>Firebase | Vercel | Netlify | GitHub | Figma | VS Code | Chrome Dev Tools. </span></span></p>
                 </div>
             </div>
